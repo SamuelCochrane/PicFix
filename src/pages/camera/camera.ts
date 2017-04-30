fab7b08ee180860b3afa9810dbbe58851dbf8035
Samuel Cochrane <SamuelDavisCochrane@Gmail.com>
2017-04-29 18:42:52 -0700

    removed deprecated pages, cleaned up code

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { FormCarPage } from '../form-car/form-car';
 import { CameraConfirmPage } from '../camera-confirm/camera-confirm';
 import { ToastController } from 'ionic-angular';
 
@@ -72,18 +72,11 @@ export class CameraPage {
      	_this.gVars.updateCurrentReport(report);
 
 
-		//console.log(report.images);
+       	//launch the camera confirm page
+       	_this.navCtrl.push(FormCarPage, {});
+      }
 
-     	//launch the camera confirm page
-     	_this.navCtrl.push(CameraConfirmPage, {});
-
-
-    }
-
-
-
-    );
-    //need to get Image out of ^ and into push
+    });
 
 
   }
